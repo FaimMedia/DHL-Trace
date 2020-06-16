57ffc9f3bfdcc2e208de5e7af1f13d510cdebc4f
Tim Peters (FaimMedia B.V.) <tim@faimmedia.nl>
2020-06-16 11:33:18 +0200

    Included content script

---
+++ src/contentscript.js
@@ -0,0 +1,15 @@
+
+const search = document.location.search;
+const hashes = search.slice(search.indexOf('?') + 1).split('&');
+const params = {};
+hashes.map(hash => {
+	const [key, val] = hash.split('=');
+
+	params[key] = decodeURIComponent(val);
+});
+
+if(typeof(params['key']) != 'undefined') {
+	if(confirm('Convert to printable page?')) {
+		document.location = chrome.runtime.getURL('trace.html') + '#' + params.key.replace('+', '/');
+	}
+}
