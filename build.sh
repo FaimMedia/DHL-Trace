4cd50c6026b4b4ec695e862ab570ec0c0f1bdb60
Tim Peters (FaimMedia B.V.) <tim@faimmedia.nl>
2020-06-23 17:43:46 +0200

    Updated build, now using Chrome

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-zip build.zip -d -T -q -9 -r src
-mv build.zip build.crx
+
+# MacOS only
+
+/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --pack-extension=./src/ --pack-extension-key=dhl-tracking.pem
+mv src.crx build.crx
